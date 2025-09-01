2e681ea52820b1ceb1fee4e93ff80e78369e4551
Siyabonga Samson Lukhele <siyabonga.lukhele@umuzi.org>
2025-09-01 13:24:57 +0200

    refactor: remove explicit _id field from User schema as it's auto-generated by mongoose

--- src/models/User.ts
+++ src/models/User.ts
@@ -13,10 +13,6 @@ export interface IUser extends Document {
 // User schema for MongoDB
 const userSchema = new mongoose.Schema(
   {
-    _id: {
-      type: mongoose.Types.ObjectId,
-      auto: true,
-    },
     username: {
       type: String,
       required: true,
