4f21cd59d203be2906aae5340369a7ae3bd3ec7e
Siyabonga Samson Lukhele <siyabonga.lukhele@umuzi.org>
2025-09-01 13:00:33 +0200

    feat: implement Activity model with validation and auto-fill for value

---
+++ src/models/Activity.ts
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+import { activityData, ActivityData } from "../utils/activity-data";
+
+type Category = keyof typeof activityData;
+type Activity<C extends Category> = keyof (typeof activityData)[C];
+
+interface ActivityDoc {
+  category: Category;
+  activity: Activity<Category>;
+  value: number;
+}
+
+const categoryEnum = Object.keys(activityData);
+const activityEnums: { [key: string]: string[] } = {};
+for (const category of categoryEnum) {
+  activityEnums[category as Category] = Object.keys(activityData[category]);
+}
+
+const activitySchema: mongoose.Schema = new mongoose.Schema(
+  {
+    category: {
+      type: String,
+      enum: categoryEnum,
+      required: true,
+    },
+    activity: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (this: any, value: string): boolean {
+          return activityEnums[this.category]?.includes(value);
+        },
+        message: (props: any): string =>
+          `${props.value} is not a valid activity for category ${props.instance.category}`,
+      },
+    },
+    value: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  { collection: "activities" }
+);
+
+// auto-fill `value` from activityData
+activitySchema.pre("validate", function (next) {
+  const category = this.category as Category;
+  const activity = this
+    .activity as keyof (typeof activityData)[typeof category];
+
+  const correctValue = activityData[category]?.[activity];
+  if (correctValue === undefined) {
+    return next(
+      new Error(
+        `Activity "${this.activity}" not found in category "${this.category}"`
+      )
+    );
+  }
+
+  this.value = correctValue;
+  next();
+});
+
+export const Activity = mongoose.model<ActivityDoc>("Activity", activitySchema);
